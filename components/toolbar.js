d8b993b7ce68bd8994e8691c51d59508856fa969
olajvde <eaolaoti@gmail.com>
2021-05-05 18:20:26 +0200

    '.'

--- components/toolbar.js
+++ components/toolbar.js
@@ -8,8 +8,8 @@ export const Toolbar = () => {
     <div className={styles.main}>
       <div onClick={() => router.push("/")}>Home</div>
       <div onClick={() => router.push("/feed/1")}>Feed</div>
-      <div onClick={() => router.push("/profile")}>Profile</div>
       <div onClick={() => router.push("/crypto")}>Crypto</div>
+      <div onClick={() => router.push("/profile")}>Profile</div>
     </div>
   );
 };
